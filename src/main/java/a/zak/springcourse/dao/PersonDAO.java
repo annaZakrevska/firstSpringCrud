e2e42b3e352794152104a5b2cba7d36a5943bc1a
annaZakrevska <anutazak2@gmail.com>
2021-12-13 23:55:02 +0100

    adding methods: find by index, show all

--- src/main/java/a/zak/springcourse/dao/PersonDAO.java
+++ src/main/java/a/zak/springcourse/dao/PersonDAO.java
@@ -1,9 +1,29 @@
 package a.zak.springcourse.dao;
 
 import a.zak.springcourse.models.Person;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonDAO {
+    private static int id_count;
     private List<Person> people;
 
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++id_count, "Tom"));
+        people.add(new Person(++id_count, "Bob"));
+        people.add(new Person(++id_count, "Mike"));
+        people.add(new Person(++id_count, "Katy"));
+    }
+
+    public List<Person> show(){
+        return people;
+    }
+
+    public Person index(int id){
+        return people.stream().filter(person->person.getId()==id).findAny().orElse(null);
+    }
 }
